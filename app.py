6c9225a95dcb92277070f73515bb2f6090fae956
cuantoio <93615377+cuantoio@users.noreply.github.com>
2023-06-15 21:50:53 -0700

    new push

--- app.py
+++ app.py
@@ -367,5 +367,5 @@ def api_combined_summary():
         return jsonify({'combined_summary': combined_summary, 'username': username})
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    # app.run(port=5000)
     app.run(host="0.0.0.0", port=8080)
