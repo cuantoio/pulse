aaa226345344278002381b9a7b3a806edd023897
cuantoio <93615377+cuantoio@users.noreply.github.com>
2023-08-15 09:41:18 -0700

    New deployment

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -7,10 +7,10 @@ aws ecr get-login-password --region us-west-2 | docker login --username AWS --pa
 docker build -t pulse-backend .
 
 # Tag Docker Image
-docker tag pulse-backend:4.4 260939427961.dkr.ecr.us-west-2.amazonaws.com/pulse-backend:4.4
+docker tag pulse-backend:4.5 260939427961.dkr.ecr.us-west-2.amazonaws.com/pulse-backend:4.5
 
 # Push Docker Image to ECR
-docker push 260939427961.dkr.ecr.us-west-2.amazonaws.com/pulse-backend:4.4
+docker push 260939427961.dkr.ecr.us-west-2.amazonaws.com/pulse-backend:4.5
 
 # Git operations
 git init
