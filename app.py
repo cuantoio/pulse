e8858cacbe689bb3091297109385fc87b5a7be24
cuantoio <93615377+cuantoio@users.noreply.github.com>
2024-02-06 20:27:28 -0800

    New deployment

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def run_test():
 
 @app.route('/eb')
 def run_eb():
-    return 'eb-live alpha tri v3.9h'
+    return 'eb-live alpha tri v3.9i'
 
 from decimal import Decimal
 from sklearn.feature_extraction.text import TfidfVectorizer
